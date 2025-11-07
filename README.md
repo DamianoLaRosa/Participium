289c19559691d5d28e025a66cc4e695e0ecc90d0
GIULIA PRICINA s281173 <s281173@studenti.polito.it>
2025-11-07 16:16:46 +0100

    partial update of the BE (still controlling)

--- README.md
+++ README.md
@@ -4,6 +4,39 @@ terminal FE : cd client;npm i;npm run dev
 connection to db: (docker-desktop open and running) docker compose up -d
 
 # mail - password
+admin@participium.local -  participium
+
+
 aaaaaaaa@aaaaaaaaaaaa.com - password
 
-# Questions
+# DB
+
+citizens
+( citizen_id, email, username, first_name, last_name, password_hash, salt, email_notifications, created_at )
+
+offices
+( office_id, name, description )
+
+operators
+( operator_id, email, username, password_hash, salt, office_id )
+
+categories
+( category_id, name, office_id )
+
+statuses
+( status_id, name )
+
+reports
+( report_id, citizen_id, category_id, office_id, status_id, title, description, latitude, longitude, anonymous, rejection_reason, created_at, updated_at )
+
+photos
+( photo_id, report_id, image_url, uploaded_at )
+
+comments
+( comment_id, report_id, operator_id, content, created_at )
+
+messages
+( message_id, report_id, sender_type, sender_id, content, sent_at )
+
+notifications
+( notification_id, citizen_id, report_id, message, sent_at, seen )
--- server/dao.mjs
+++ server/dao.mjs
@@ -1,4 +1,3 @@
-import {  } from './models.mjs';   
 import { Pool } from 'pg';
 import crypto from 'crypto';
 
@@ -16,9 +15,9 @@ export const getUser = async (username, password) => {
     const result = await pool.query(sql, [username]);
 
     const row = result.rows[0];
-    if (!row) return false;
+    if (!row) return await getOperator(username, password);
 
-    const user = { id: row.citizen_id, username: row.username };
+    const user = { id: row.citizen_id, username: row.username, type: "user" };
 
     return new Promise((resolve, reject) => {
       crypto.scrypt(password, row.salt, 32, (err, hashedPassword) => {
@@ -37,20 +36,81 @@ export const getUser = async (username, password) => {
   }
 };
 
+export const getOperator = async (username, password) => {
+  try {
+    const sql = 'SELECT * FROM "operators" WHERE email = $1';
+    const result = await pool.query(sql, [username]);
 
-export const createUser = async (username, email,first_name,last_name, password) => {
+    const row = result.rows[0];
+    if (!row) return false;
+
+    const user = { id: row.operator_id, username: row.username, type: "operator" };
+
+    return new Promise((resolve, reject) => {
+      crypto.scrypt(password, row.salt, 32, (err, hashedPassword) => {
+        if (err) return reject(err);
+
+        const match = crypto.timingSafeEqual(
+          Buffer.from(row.password_hash, 'hex'),
+          hashedPassword
+        );
+
+        resolve(match ? user : false);
+      });
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const createUser = async (username, email,first_name,last_name , email_notifications, password) => {
   const salt = crypto.randomBytes(16).toString('hex');
 
   return new Promise((resolve, reject) => {
     crypto.scrypt(password, salt, 32, async (err, hashedPassword) => {
       if (err) return reject(err);
 
-      const sql = 'INSERT INTO "citizens"(username, email,first_name,last_name, password_hash, salt) VALUES($1, $2, $3, $4, $5, $6) RETURNING citizen_id';
-      const values = [username, email,first_name,last_name, hashedPassword.toString('hex'), salt];
+      const sql = 'INSERT INTO "citizens"(username, email,first_name,last_name, password_hash,email_notifications, salt) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING citizen_id';
+      const values = [username, email,first_name,last_name, hashedPassword.toString('hex'),email_notifications, salt];
 
       try {
         const result = await pool.query(sql, values);
-        resolve({ id: result.rows[0].citizen_id, username });
+        resolve({ id: result.rows[0].citizen_id, username: username , first_name: first_name });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+};
+
+export const getAllOffices = async () => {
+  try {
+    const sql = 'SELECT * FROM offices';
+    const result = await pool.query(sql);
+    return result.rows.map((e) => {return { id: e.office_id, name: e.name};});
+  } catch (err) {
+    throw err;
+  }
+};
+
+
+export const createOperator = async (email, username, password, office_id) => {
+  const salt = crypto.randomBytes(16).toString('hex');
+
+  return new Promise((resolve, reject) => {
+    crypto.scrypt(password, salt, 32, async (err, hashedPassword) => {
+      if (err) return reject(err);
+
+      const sql = `
+        INSERT INTO operators (email, username, password_hash, salt, office_id)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING operator_id
+      `;
+      const values = [email, username, hashedPassword.toString('hex'), salt, office_id];
+
+      try {
+        const result = await pool.query(sql, values);
+        resolve({ id: result.rows[0].operator_id, username });
       } catch (err) {
         reject(err);
       }
@@ -59,9 +119,17 @@ export const createUser = async (username, email,first_name,last_name, password)
 };
 
 
-/*
 
-const { Client } = require('pg');
+
+
+
+
+
+
+
+
+
+/*
 require('dotenv').config();
 
 const client = new Client({
@@ -75,13 +143,4 @@ client.connect()
 
 module.exports = client;
 // LA VARIABILE client E' L'EQUIVALENTE DEL db DI SQLITE3
-
-------ESEMPIO DI QUERY ------
-
-client.query('SELECT * FROM citizens', (err, res) => {
-  if (err) throw err;
-  console.log(res.rows);
-});
-
-
 */
